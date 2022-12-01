618c9f03ebc55ce504d6a0ceef05e39cb71d42be
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-11-30 17:36:07 -0800

    practice with position property

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,35 +2,44 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-  return <View style={styles.viewStyle}>
-    <Text style={styles.textOneStyle}>Child #1</Text>
-    <Text style={styles.textTwoStyle}>Child #2</Text>
-    <Text style={styles.textThreeStyle}>Child #3</Text>
+  return <View style={styles.parentStyle}>
+    <View style={styles.viewOneStyle} />
+    <View style={styles.viewTwoParent}>
+      <View style={styles.viewTwoStyle} />
+    </View>
+    <View style={styles.viewThreeStyle} />
   </View>
 };
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
     borderColor: 'black',
     // default for alignItems: 'stretch'
     // default for flexDirection: 'column'
     // alignItems: 'flex-start'
     height: 200,
-    alignItems: 'center'
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
-  textOneStyle: {
-    borderWidth: 3,
-    borderColor: 'red'
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red',
   },
-  textTwoStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
-    alignSelf: 'stretch'
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
   },
-  textThreeStyle: {
-    borderWidth: 3,
-    borderColor: 'red'
+  viewTwoParent: {
+    height: 100,
+    justifyContent: 'flex-end'
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple',
   }
 });
 
